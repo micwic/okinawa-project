8ac40fee2e8d02f5fee5ee949d864b4169e0ec2b
Michel Wicky <michel.wicky@osima.ch>
2017-12-27 18:16:30 +0100

    Initial setup of the client application based on ultima theme from primeng components framework

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MessageComponent } from './shared/message/message.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MessageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
